bc2196e70b2641fb2c7ebc8d5344e5570d39b37c
fabischapeli <fabischapeli97@gmail.com>
2017-11-23 10:55:54 +0300

    implementatation of save and delete contact tests

--- contact.py
+++ contact.py
@@ -4,6 +4,39 @@ class Contact:
     """
 
     contact_list = []
+    # Init method up here
+    def save_contact(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Contact.contact_list.append(self)
+
+    def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
+
+@classmethod
+    def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+
+        Args:
+            number: Phone number to search for
+        Returns:
+            Contact of person that matches the numberself.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
+
 
     def __init__(self,first_name,last_name,number,email):
 
--- contact_test.py
+++ contact_test.py
@@ -26,7 +26,57 @@ def setUp(self):
         self.assertEqual(self.new_contact.phone_number, "0712345678")
         self.assertEqual(self.new_contact.email,"james@gmail.com")
 
+    def test_save_contact(self):
+        '''
+        test_save_contact test case to test if the contact object is saves into the contact list
+        '''
+        self.new_contact.save_contact() #saving the new contact
+        self.assertEqual(len(Contact.contact_list,1))
 
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Contact.contact_list = []
+
+    def test_save_multiple_contact(self):
+        '''
+        test_save_multiple_contact to check if we can save multiple contact
+        objects to our contact_list
+        '''
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0712345678","test@user.com") #new contact
+
+        test_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list),2)
+
+    def test_delete_contact(self):
+        '''
+        test_delete_contact to test if we can remove a contact from our
+        contact list
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+
+        test_contact.save_contact()
+
+        self.new_contact.delete_contact() # deleting a contact object
+        self.assertEqual(len(Contact.contact_list),1)
+
+    def test_find_contact_by_number(self):
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+
+        test_contact.save_contact()
+        found_contact = Contact.find_by_number("0711223344")
+
+        self.assertEqual(found_contact.email,test_contact.email)
+        
 
 if __name__ == '_main_':
     unittest.main()
